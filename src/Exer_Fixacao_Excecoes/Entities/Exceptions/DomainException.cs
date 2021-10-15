6ba3061ab3e1b28f5945d028b1226da38ff26da2
RaulORios <raul.orios89@outlook.com>
2021-10-15 14:09:18 -0300

    Arquivos atualizados

---
+++ src/Exer_Fixacao_Excecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exer_Fixacao_Excecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
